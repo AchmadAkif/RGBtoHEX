1ebf5195f374b14e642cecd596d7c266f1d9d701
achmadakif <achmadakif7@gmail.com>
2023-09-14 20:39:22 +0700

    fixed 0 value considered undefined

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-//RGB to HEX color code
-//rgb(a, b, c)
-const counting = {1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:'A', 11:'B', 12:'C', 13:'D', 14:'E', 15:'F'}
+//RGB to HEX color code achmadakif 14.09.23
+
+const counting = {
+  0:0, 1:1,
+  2:2, 3:3,
+  4:4, 5:5,
+  6:6, 7:7,
+  8:8, 9:9,
+  10:'A', 11:'B', 
+  12:'C', 13:'D', 
+  14:'E', 15:'F'
+}
 let hex = '';
 
 let rgbRed = prompt('masukkan kode RED');
@@ -11,10 +20,8 @@ function convertRed (r) {
   //Get the decimal part then multiply by 16
   let decimalPart = (division - floored) * 16;
   //Find HEX value in Object
-  hex += counting[floored] + counting[decimalPart]
-
-  console.log(floored)
-  console.log(decimalPart)
+  hex += counting[floored]
+  hex += counting[decimalPart] 
 }
 convertRed(rgbRed)
 
@@ -24,10 +31,8 @@ function convertGreen (g) {
   let floored = Math.floor(division);
   let decimalPart = (division - floored) * 16;
 
-  hex += counting[floored] + counting[decimalPart]
-
-  console.log(floored)
-  console.log(decimalPart)
+  hex += counting[floored] 
+  hex += counting[decimalPart]
 }
 convertGreen(rgbGreen)
 
@@ -37,10 +42,8 @@ function convertBlue (b) {
   let floored = Math.floor(division);
   let decimalPart = (division - floored) * 16;
 
-  hex += counting[floored] + counting[decimalPart]
-
-  console.log(floored)
-  console.log(decimalPart)
+  hex += counting[floored] 
+  hex += counting[decimalPart]
 }
 convertBlue(rgbBlue)
 
